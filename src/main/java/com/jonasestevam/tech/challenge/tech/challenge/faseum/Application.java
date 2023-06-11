8d5c4e3d117736c34f8a5538f343e5af5785c82c
Jonas Estevam <jonas_estevam@live.com>
2023-06-10 23:50:05 -0300

    initial commit

---
+++ src/main/java/com/jonasestevam/tech/challenge/tech/challenge/faseum/Application.java
@@ -0,0 +1,13 @@
+package com.jonasestevam.tech.challenge.tech.challenge.faseum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
